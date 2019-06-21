e678c996a718e11b289347367f3d332984c1a6bd
Vladimir Stepanov <vovan.number.one@mail.ru>
2019-06-21 12:24:34 +0400

    first commit

---
+++ ApiVkProject/Models/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.EntityFrameworkCore.Extensions;
+
+namespace ApiVkProject.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ApiVkProject/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiVkProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Photo { get; set; }
+        //public bool Online { get; set; }
+        //public DateTime BirthDate { get; set; }
+    }
+}
